329af91b1105d5cc3e4253f93884c396aacde1e8
Nguyễn Thị Thanh Trúc <anhkhoanguyenhoang14042004@gmail.com>
2025-05-07 09:15:43 +0700

    backup - github

--- WebsiteBanXeMay_Nhom3/Models/SD.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebsiteBanXeMay_Nhom3.Models
-{
-    public static class SD
-    {
-        public const string Role_Customer = "Customer";
-        public const string Role_Admin = "Admin";
-        public const string Role_Employee = "Employee";
-    }
-}
---
+++ WebsiteCoffeeShop/Repositories/DiscountCodeRepositories/DiscountCodeRepository.cs
@@ -0,0 +1,47 @@
+﻿using WebsiteCoffeeShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebsiteCoffeeShop.Repositories.DiscountCodeRepositories
+{
+    public class DiscountCodeRepository : IDiscountCodeRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DiscountCodeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<DiscountCode>> GetAllAsync()
+            => await _context.DiscountCodes.ToListAsync();
+
+        public async Task<DiscountCode> GetByIdAsync(int id)
+            => await _context.DiscountCodes.FindAsync(id);
+
+        public async Task<DiscountCode> GetByCodeAsync(string code)
+            => await _context.DiscountCodes.FirstOrDefaultAsync(x => x.Code == code);
+
+        public async Task AddAsync(DiscountCode discountCode)
+        {
+            _context.DiscountCodes.Add(discountCode);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(DiscountCode discountCode)
+        {
+            _context.DiscountCodes.Update(discountCode);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var discount = await GetByIdAsync(id);
+            if (discount != null)
+            {
+                _context.DiscountCodes.Remove(discount);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+
+}
